6081eda341fe02848f22a910bb21bc533f992be1
Axelito181 <78041818+Axelito181@users.noreply.github.com>
2021-05-05 10:08:44 +0200

    Initial commit

---
+++ Assets/CloneZone.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneZone : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) {
+        Instantiate(other.gameObject);
+    }
+}
---
+++ Assets/KillZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    void OnTriggerEnter (Collider other) {
+        // Debug.Break ();
+        // Debug.Log(other);
+        Destroy(other.gameObject) ;
+
+    }
+        
+}
